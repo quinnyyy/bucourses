2e7e4f40ec611d7cec0d24d54620a5a1813a5a78
Quinn Meurer <qmeurer@mathworks.com>
2019-07-09 17:16:56 -0400

    Scrape all course names and print to screen

--- scraper.py
+++ scraper.py
@@ -1,13 +1,47 @@
 import requests
-import urllib.request
-import time
 from bs4 import BeautifulSoup
 
-url = 'https://www.bu.edu/academics/cas/courses/'
-response = requests.get(url)
-soup = BeautifulSoup(response.text, "html.parser")
-courseFeedDiv = soup.find("ul", {"class": "course-feed"})
 
-for li in courseFeedDiv.find_all('li', recursive=False):
-    name = li.find('a').find('strong')
-    print(name.contents)
+def getSoup(url):
+    response = requests.get(url)
+    soup = BeautifulSoup(response.text, "html.parser")
+    return soup
+
+
+def getNumPages(url):
+    soup = getSoup(url)
+    numPagesDiv = soup.find("div", {"class": "pagination"})
+    if numPagesDiv is None:
+        return 1
+    else:
+        return int(numPagesDiv.find_all("span")[-1].find("a").contents[0])
+
+
+def getClassInfoFromPage(url):
+    soup = getSoup(url)
+    courseFeedDiv = soup.find("ul", {"class": "course-feed"})
+
+    for li in courseFeedDiv.find_all('li', recursive=False):
+        name = li.find('a').find('strong')
+        print(name.contents)
+
+
+colleges = ['khc', 'cas', 'com', 'eng', 'cfa', 'cgs', 'sar', 'gms', 'grs',
+            'sdm', 'met', 'questrom', 'sha', 'law', 'busm', 'sph', 'ssw',
+            'sth', 'wheelock']
+
+numPages = []
+
+urlFront = 'https://www.bu.edu/academics/'
+urlBack = '/courses/'
+
+for college in colleges:
+    numPages.append(getNumPages(urlFront + college + urlBack))
+
+for i, college in enumerate(colleges):
+    url = urlFront + college + urlBack
+    for pageNumber in range(1, numPages[i] + 1):
+        getClassInfoFromPage(url + str(pageNumber))
+
+
+print('done')
