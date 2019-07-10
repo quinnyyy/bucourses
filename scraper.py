8b820140564858b0852252bb331a1432449ae247
Quinn Meurer <quinnyyy@bu.edu>
2019-07-10 01:11:36 -0400

    Write every course name to text file classNames.txt from scraper.py

--- scraper.py
+++ scraper.py
@@ -1,5 +1,6 @@
 import requests
 from bs4 import BeautifulSoup
+from functools import reduce
 
 
 def getSoup(url):
@@ -20,28 +21,46 @@ def getNumPages(url):
 def getClassInfoFromPage(url):
     soup = getSoup(url)
     courseFeedDiv = soup.find("ul", {"class": "course-feed"})
-
+    classes = []
     for li in courseFeedDiv.find_all('li', recursive=False):
         name = li.find('a').find('strong')
-        print(name.contents)
+        classes.append(name.contents)
+    classes = reduce(lambda x, y: x + y, classes)
+    return classes
 
 
-colleges = ['khc', 'cas', 'com', 'eng', 'cfa', 'cgs', 'sar', 'gms', 'grs',
-            'sdm', 'met', 'questrom', 'sha', 'law', 'busm', 'sph', 'ssw',
-            'sth', 'wheelock']
+if __name__ == "__main__":
 
-numPages = []
+    colleges = ['khc', 'cas', 'com', 'eng', 'cfa', 'cgs', 'sar', 'gms', 'grs',
+                'sdm', 'met', 'questrom', 'sha', 'law', 'busm', 'sph', 'ssw',
+                'sth', 'wheelock']
+    numPages = []
+    classListByCollege = []
 
-urlFront = 'https://www.bu.edu/academics/'
-urlBack = '/courses/'
+    urlFront = 'https://www.bu.edu/academics/'
+    urlBack = '/courses/'
 
-for college in colleges:
-    numPages.append(getNumPages(urlFront + college + urlBack))
+    f = open("classNames.txt", "w")
 
-for i, college in enumerate(colleges):
-    url = urlFront + college + urlBack
-    for pageNumber in range(1, numPages[i] + 1):
-        getClassInfoFromPage(url + str(pageNumber))
+    for college in colleges:
+        print("Getting page number for " + college)
+        numPages.append(getNumPages(urlFront + college + urlBack))
 
+    for i, college in enumerate(colleges):
+        print("Getting classes for " + college)
+        url = urlFront + college + urlBack
+        classes = []
+        for pageNumber in range(1, numPages[i] + 1):
+            name = getClassInfoFromPage(url + str(pageNumber))
+            classes.append(name)
+        classes = reduce(lambda x, y: x + y, classes)
+        classListByCollege.append(classes)
 
-print('done')
+    print('Writing classes to file...')
+    for classList in classListByCollege:
+        for className in classList:
+            f.write(className + "\n")
+
+    f.close()
+
+    print('done')
