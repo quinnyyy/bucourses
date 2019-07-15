f551fc0bc8b4e6ad6692239349bb24ea3da04d95
quinnyyy <quinnyyy@bu.edu>
2019-07-15 10:42:30 -0400

    Building api in /server. Connected to mongo and basic get endpoint for classes

---
+++ server/src/server.ts
@@ -0,0 +1,28 @@
+import express = require('express');
+import { MongoHelper } from './mongoHelper';
+
+const app: express.Application = express();
+var courseInfo: any;
+
+
+app.get('/class', (req, res) => {
+    let code: string | undefined = req.query.code;
+
+    courseInfo.findOne({Code: code})
+        .then((doc: any) => {
+            if (!doc)
+                throw new Error('No record found');
+            res.send(doc);
+        })
+});
+
+app.listen(3000, async () => {
+    console.log('example app listening on port 3000!');
+    try {
+        let connection: any = await MongoHelper.connect();
+        courseInfo = connection.db('bucourses_db').collection('course_info');
+        console.log('connection successful :)');
+    } catch(err) {
+        console.error('whoops!', err);
+    }
+});
