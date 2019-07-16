219f3b3100d06d88bbbca4dfc6f8e4cfd3624501
quinnyyy <quinnyyy@bu.edu>
2019-07-16 09:53:33 -0400

    Adding bootstrap theme to frontend folder

--- public2/src/index.js
+++ public2/src/index.js
@@ -2,8 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import '../growth/css/bootstrap4-growth.min.css'
 
-const title = 'My Minimal React Webpack Babel Setup';
+const title = "Hello world I'm so fat";
 
 ReactDOM.render(
   <App title={title} />,
--- public2/webpack.config.js
+++ public2/webpack.config.js
@@ -8,6 +8,14 @@ module.exports = {
         test: /\.(js|jsx)$/,
         exclude: /node_modules/,
         use: ['babel-loader']
+      },
+      {
+        test: /\.css$/,
+        use: ['style-loader','css-loader']
+      },
+      {
+        test: /\.(jpe?g|png|gif|woff|woff2|eot|ttf|svg)(\?[a-z0-9=.]+)?$/,
+        use: ['url-loader?limit=100000']
       }
     ]
   },
